8576532ce61879b313535000318fff025272ac67
Rafeef Alghamdi <115750786+Rie0@users.noreply.github.com>
2024-08-12 14:28:40 +0300

    Delete src directory

--- src/main/java/org/twspring/lab10/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.twspring.lab10.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.twspring.lab10.Model.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
